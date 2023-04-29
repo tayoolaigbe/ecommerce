75fa70d5e8fb7cebcd8085a78698e2dc1867fe70
tayoolaigbe <tayoolaigbe@outlook.com>
2023-04-29 08:45:16 +0100

    Successully added mongoDB to the admin project

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,13 +1,17 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import clientPromise from '@/lib/mongodb';
+
 export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    // ...add more providers here
-  ],
-}
-export default NextAuth(authOptions)
+	// Configure one or more authentication providers
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+		}),
+		// ...add more providers here
+	],
+	adapter: MongoDBAdapter(clientPromise),
+};
+export default NextAuth(authOptions);
